c3e1d3058cef0b5356624e14e4fc2a28a600522d
Yoann Le Coguic <yoann.lecoguic@gmail.com>
2016-02-16 11:29:59 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'gextra'
+});
+
+app.get('/', function (req, res) {
+	connection.query('SELECT * FROM etablissement', function(err, rows, fields) {
+		if (err)
+			res.status(500).send(err);
+
+		res.status(200).send('The solution is: ', rows);
+	});
+});
+
+app.listen(9520, function () {
+	console.log('Example app listening on port 9520!');
+});
